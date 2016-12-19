8e04ebe6ab7073d6647b8f88da211638af60ee5d
alexanderwjrussell <awjrussell@live.co.uk>
2016-12-19 11:05:50 +0000

    Initial Setup 1) Project setup 2) All tests written

---
+++ RomanNumerals_CS/RomanNumerals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RomanNumerals_CS
+{
+	public static class RomanNumerals
+	{
+		public static string Add(string value1, string value2)
+		{
+			return null;
+		}
+	}
+}
---
+++ RomanNumerals_CS/RomanNumeralsTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+namespace RomanNumerals_CS
+{
+	[TestFixture]
+	public class RomanNumeralsTest
+	{
+		[TestCase("I", "I", "II")]
+		[TestCase("I", "II", "III")]
+		[TestCase("II", "II", "IV")]
+		[TestCase("IIII", "I", "V")]
+		[TestCase("V", "I", "VI")]
+		[TestCase("V", "V", "X")]
+		[TestCase("XVI", "XVI", "XXXII")]
+		[TestCase("XX", "XXX", "L")]
+		[TestCase("L", "L", "C")]
+		[TestCase("CLI", "CCCV", "CCCCLVI")]
+		[TestCase("CCC", "CC", "D")]
+		[TestCase("D", "D", "M")]
+		[TestCase("MD", "DXVI", "MMXVI")]
+		public void Should_ReturnCorrectValue(string value1, string value2, string result)
+		{
+			Assert.That(RomanNumerals.Add(value1, value2), Is.EqualTo(result));
+		}
+	}
+}
