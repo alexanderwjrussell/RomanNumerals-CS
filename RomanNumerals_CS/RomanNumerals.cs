3d7c19e2f0e79dc74897e2ea591cf666676f0c77
alexanderwjrussell <awjrussell@live.co.uk>
2016-12-19 11:21:04 +0000

    Initial V substitution created

--- RomanNumerals_CS/RomanNumerals.cs
+++ RomanNumerals_CS/RomanNumerals.cs
@@ -6,8 +6,26 @@ namespace RomanNumerals_CS
 	{
 		public static string Add(string value1, string value2)
 		{
-			var answer = value1 + value2;
-			return answer;
+			var answer = AddIs(value1, value2);
+			return ConvertFromIs(answer);
+		}
+
+		public static string AddIs(string value1, string value2)
+		{
+			var total = ConvertToIs(value1) + ConvertToIs(value2);
+			return total;
+		}
+
+		public static string ConvertToIs(string number)
+		{
+			number = number.Replace("V", "IIIII");
+			return number;
+		}
+
+		public static string ConvertFromIs(string number)
+		{
+			number = number.Replace("IIIII", "V");
+			return number;
 		}
 	}
 }
